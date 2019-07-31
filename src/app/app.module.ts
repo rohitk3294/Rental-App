a0f5e41c367cdc0b58f0d215ac103f0d2d4a0108
Rohit Kumar <skyrohitkumar@gmail.com>
2019-07-31 11:55:19 +0530

    add rental list item and detail component and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 
+
 import { HeaderComponent } from './common/header/header.component';
+import { RentalModule } from './rental/rental.module';
+
+
+
+const routes: Routes = [
+{path: '', redirectTo: '/rentals' , pathMatch: 'full'},
+]
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+ 
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rental/shared/rental.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs/Rx"
+
+import {Rental} from "../shared/rental.model"
+
+@Injectable()
+export class RentalService {
+
+  private  rentals: Rental[] = [{
+        id: "1",
+        title: "Central Apartment",
+        city: "Noida",
+        street: "Times Sqaure",
+        category: "apartment",
+        image: "http://via.placeholder.com/350x250",
+        bedrooms: 3,
+        description: "Very nice apartment",
+        dailyRate: 34,
+        shared: false,
+        createdAt: "24/12/2017"
+      },
+      {
+        id: "2",
+        title: "Central Apartment 2",
+        city: "Delhi",
+        street: "Main street",
+        category: "apartment",
+        image: "http://via.placeholder.com/350x250",
+        bedrooms: 2,
+        description: "Very nice apartment",
+        dailyRate: 12,
+        shared: true,
+        createdAt: "24/12/2017"
+      },
+      {
+        id: "3",
+        title: "Central Apartment 3",
+        city: "Agra",
+        street: "Hlavna",
+        category: "house",
+        image: "http://via.placeholder.com/350x250",
+        bedrooms: 2,
+        description: "Very nice apartment",
+        dailyRate: 334,
+        shared: true,
+        createdAt: "24/12/2017"
+      },
+      {
+        id: "4",
+        title: "Central Apartment 4",
+        city: "Mumbai",
+        street: "Haupt strasse",
+        category: "house",
+        image: "http://via.placeholder.com/350x250",
+        bedrooms: 9,
+        description: "Very nice apartment",
+        dailyRate: 33,
+        shared: true,
+        createdAt: "24/12/2017"
+    }];
+
+
+    public getRentalById(rentalId: string): Observable<Rental> {
+     return  new Observable((observer) => {
+            setTimeout(() =>{
+              const foundRental= this.rentals.find((rental)=> {
+              return rental.id == rentalId;
+                }); 
+                observer.next(foundRental);       
+            }, 500);
+        });
+      }
+
+     public getRentals(): Observable<Rental[]>{
+     return new Observable<Rental[]>((observer) => {
+       /*  Here we call Asyncrounous Function with Defining observers to understand Observer*/
+        setTimeout(() =>{
+            observer.next(this.rentals);
+        }, 1000);
+
+
+    /* For only learning purpose   
+       setTimeout(() =>{
+            observer.error("Error");
+        }, 2000);
+        setTimeout(() =>{
+            observer.complete();
+        }, 3000);
+     */
+
+    });
+    //   return rentalObservable;
+    }
+}
+
